c6a68fb242c5dddc4af545779f1b4de256c379b4
seabAu <79596199+seabAu@users.noreply.github.com>
2023-03-10 01:25:48 -0500

    Publish to github pages.

--- src/components/DashboardContent.js
+++ src/components/DashboardContent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Heading, Text, Pane, Spinner, Dialog } from "evergreen-ui";
+// import { Heading, Text, Pane, Spinner, Dialog } from "evergreen-ui";
 import { copyToClipboard } from "copy-lite";
 import Table from "./Table/Table";
 import Loader from "./Loader";
